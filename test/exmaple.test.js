3ef3f6ab81d42c477af9dcca1a07c6b0c455be3b
weerapon <weerapon@thinknet.co.th>
2017-03-23 08:31:40 +0700

    add exmale file test

---
+++ test/exmaple.test.js
@@ -0,0 +1,14 @@
+// Describe what to test
+describe('Test Calculate', () => {
+
+  // Test scenario
+  it('sum number', () => {
+    const x = 10;
+    const y = 15;
+    const total = x + y;
+    const expected = 25;
+    expect(total).toBe(expected)
+  })
+  // Test scenario
+
+});
