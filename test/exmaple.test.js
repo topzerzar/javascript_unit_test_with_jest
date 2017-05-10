d5995dd4296adf153c82e7256f73aee212c7cfc9
weerapon <weerapon@thinknet.co.th>
2017-05-10 15:18:18 +0700

    test 1 scenario by function test

--- test/exmaple.test.js
+++ test/exmaple.test.js
@@ -1,9 +1,12 @@
-// Describe what to test
-describe('Test Calculate', () => {
+// // Describe what to test
+// describe('Test Calculate', () => {
 
-  // Test scenario
-  it('two plus two is four', () => {
-    expect(2+2).toBe(4)
-  })
-  // Test scenario
-});
+//   // Test scenario
+//   it('two plus two is four', () => {
+//     expect(2+2).toBe(4)
+//   })
+//   // Test scenario
+// });
+test('two plus two is four', () => {
+  expect(2+2).toBe(4)
+})
